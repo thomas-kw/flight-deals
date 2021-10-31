9bb36ea016ad33f7307409775f054492695f76f0
Thomas <whelan.thomas@gmail.com>
2021-10-31 21:17:25 +0900

    building flight search module

--- flight_search.py
+++ flight_search.py
@@ -1,18 +1,31 @@
 import requests
 
-TEQUILA_ENDPOINT = "tequila-api.kiwi.com/"
+TEQUILA_ENDPOINT = "http://tequila-api.kiwi.com/"
 TEQUILA_API_KEY = "ewavzYF5kS0frIaQMDl9d6D0IpNm-d3l"
 
+headers = {
+    "apikey": TEQUILA_API_KEY
+}
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
+    # This class is responsible for talking to the Flight Search API.
 
-    def get_destination_code(self, city_name):
+    def get_destination_code(self, cities):
+        city_codes = []
+        for city in cities:
+            response = requests.get(
+                url=f"{TEQUILA_ENDPOINT}locations/query",
+                params={"term": city},
+                headers=headers
+            )
+            city_data = response.json()
+            city_codes.append(city_data["locations"][0]["code"])
+        return city_codes
 
 
+city_list = ["Paris", "London", "Tokyo"]
 
-
-
-
-        # code = "TESTING"
-        # return code
-
+flight_search = FlightSearch()
+list_1 = flight_search.get_destination_code(city_list)
+print(list_1)
